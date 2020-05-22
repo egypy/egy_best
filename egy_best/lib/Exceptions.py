36e64d2ef664add429f2dc28d7ee0bb24788f83a
n0x1s <ilyasselfatimy9@gmail.com>
2020-05-22 05:06:01 +0100

    new exceptions added

--- egy_best/lib/Exceptions.py
+++ egy_best/lib/Exceptions.py
@@ -2,3 +2,10 @@ class BotDetectedException(Exception):
 	def __init__(self):
 		msg = 'Bot Has Been detected, Try using a proxy or change the headers'
 		super().__init__(msg)
+
+class TranslateNotFound(Exception):
+	def __init__(self):
+		msg = """The word you are looking for not found in the directory,
+		please consider using the online traslator method
+		"""
+		super().__init__(msg)
