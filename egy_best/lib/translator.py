1fcc099ae11082b3d117edb8fb7a60d1f44120e1
n0x1s <ilyasselfatimy9@gmail.com>
2020-05-22 10:21:50 +0100

    updated translate method

--- egy_best/lib/translator.py
+++ egy_best/lib/translator.py
@@ -1,4 +1,5 @@
 from .exceptions import TranslateNotFound
+
 class Translator:
 	""" simple translate class to convert a word from a lang to en """
 
@@ -13,15 +14,20 @@ class Translator:
 		'التقييم': 'rating',
 		'المسلسل': 'serie',
 		'تقييم المسلسل': 'serie_rating',
+		'القصة': 'story',
 	}
 
 	@classmethod
-	def translate(cls, elem: str, *, type='offline') -> str:
+	def translate(cls, elem: str, *, type='offline', reverse=False) -> str:
 		if type == 'offline':
-			return cls.offline_translate(elem)
+			return cls.offline_translate(elem, reverse)
 
 	@classmethod
-	def offline_translate(cls, elem):
+	def offline_translate(cls, elem, reverse):
+		if reverse:
+			for name, value in cls.data.items():
+				if value == elem:
+					return name
 		if elem in cls.data:
 			return cls.data[elem]
 		raise TranslateNotFound
