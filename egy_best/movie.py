d20e546d6ffd696aaa98a30f25400934493f8cd0
n0x1s <ilyasselfatimy9@gmail.com>
2020-05-23 05:44:07 +0100

    added some methods edited the __repr__

--- egy_best/movie.py
+++ egy_best/movie.py
@@ -1,5 +1,6 @@
 from material import Material
 from lib.translator import Translator
+
 class Movie(Material):
 	""" a movie class to handle movies """
 	def __init__(self, link):
@@ -9,7 +10,15 @@ class Movie(Material):
 		self.story = self.get_story()
 		self.actors = self.get_actors()
 		self.thriller = self.get_thriller()
+		self.download_info = self.get_download_info()
 
-m = Movie('https://room.egybest.name/movie/the-dark-knight-2008/')
-r = m.get_thriller()
-print(r)
+	def __str__(self):
+		return f'{self.name} ({self.year})'
+
+	def __repr__(self):
+		return str(self)
+
+	def get_similar(self):
+		""" get similar movies """
+		container = self.soup.find(class_='contents movies_small')
+		return [Movie(link['href']) for link in container.find_all('a')]
