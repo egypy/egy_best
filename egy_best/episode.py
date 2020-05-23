f74ca7ec132dd6a5af0ee41149492292f5fd7150
n0x1s <ilyasselfatimy9@gmail.com>
2020-05-23 05:45:36 +0100

    added episode class

---
+++ egy_best/episode.py
@@ -0,0 +1,28 @@
+import re
+from serie import Serie
+from season import Season
+from material import Material
+from lib.utils import Utils
+
+class Episode(Material):
+	def __init__(self, link):
+		self.link = link
+		self.soup = Utils.page_downloader(link)
+		self.season = Season(self.get_serie_from_episode())
+		self.download_info = self.get_download_info()
+		self.episode_number = self.find_order('ep')
+
+		for name, value in self.season.serie.__dict__.items():
+			if name != 'soup':
+				setattr(self, name, value)
+
+	def find_order(self, pram):
+		return  re.search(f'{pram}-(.*)/', self.link).group(1)
+
+	def get_serie_from_episode(self):
+		order = self.find_order('ep')
+		return self.link.replace(f'-ep-{order}', '').replace('episode', 'season')
+
+
+e = Episode('https://room.egybest.name/episode/mr-robot-season-4-ep-7/?ref=search-p1')
+print(e.episode_number)
