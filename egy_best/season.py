014d05a7be95c30ba14cedc330593c02e2e7ca12
n0x1s <ilyasselfatimy9@gmail.com>
2020-05-24 08:35:26 +0100

    auto selcet class

--- egy_best/season.py
+++ egy_best/season.py
@@ -1,17 +1,19 @@
 import re
 from serie import Serie
+from lib.utils import Utils
 
 class Season(Serie):
 	def __init__(self, link):
 		super().__init__(link)
 		self.episodes = self.get_episodes()
 		self.season_number = self.find_order('season')
-		self.serie = Serie(self.get_serie_from_season())
+		self.serie = Serie((self.get_serie_from_season()))
 
 	def get_episodes(self):
 		""" get all episodes of a Season """
 		container = self.soup.find(class_='movies_small')
-		return [link['href'] for link in container.find_all('a')]
+		return [Utils.pickup_class(link['href'])
+		 	for link in container.find_all('a')]
 
 	def find_order(self, pram):
 		return  re.search(f'{pram}-(.*)/', self.link).group(1)
