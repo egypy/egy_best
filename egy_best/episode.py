b0f0ca8f0c36e8f27d191b14b6479edf9e0c9c78
n0x1s <ilyasselfatimy9@gmail.com>
2020-06-19 14:12:05 +0100

    updated the Actor class to the new style reduce internet consumption

--- egy_best/episode.py
+++ egy_best/episode.py
@@ -3,18 +3,15 @@ from serie import Serie
 from season import Season
 from material import Material
 from lib.utils import Utils
-
 class Episode(Material):
-	def __init__(self, link):
+	def __init__(self, link, **kwargs):
 		self.link = link
-		self.soup = Utils.page_downloader(link)
-		self.season = Season(self.get_serie_from_episode())
-		self.download_info = self.get_download_info()
-		self.episode_number = self.find_order('ep')
-
-		for name, value in self.season.serie.__dict__.items():
-			if name != 'soup':
+		super().__init__(link, **kwargs)
+		if self.access:
+			for name, value in self.season.serie.__dict__.items():
 				setattr(self, name, value)
+			self.soup = Utils.page_downloader(link)
+
 
 	def find_order(self, pram):
 		return  re.search(f'{pram}-(.*)/', self.link).group(1)
@@ -23,3 +20,21 @@ class Episode(Material):
 		order = self.find_order('ep')
 		r = self.link.replace(f'-ep-{order}', '').replace('episode', 'season')
 		return r
+
+	@property
+	def season(self):
+		if not hasattr(self, '_season'):
+			self._season = Season(link=self.get_serie_from_episode())
+		return self._season
+
+	@property
+	def download_info(self):
+		if not hasattr(self, '_download_info'):
+			setattr(self, '_download_info', self.get_download_info())
+		return self._download_info
+
+	@property
+	def episode_number(self):
+		if not hasattr(self, '_episode_number'):
+			setattr(self, '_episode_number', self.find_order('ep'))
+		return self._episode_number
