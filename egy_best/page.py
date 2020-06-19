8e75f9231288322afee936ee7022caceb87f4a16
n0x1s <ilyasselfatimy9@gmail.com>
2020-06-19 14:09:23 +0100

    updated the Actor class to the new style reduce internet consumption added some properties

--- egy_best/page.py
+++ egy_best/page.py
@@ -1,7 +1,35 @@
 from lib.utils import Utils
+from lib.settings import Settings
+
 class Page:
 	""" a class to handle html pages on the site """
-	def __init__(self, link):
+	def __init__(self, link, **kwargs):
 		self.link = link
-		self.soup = Utils.page_downloader(link)
 		self.page_type = Utils.page_type(link)
+		self.access = Settings.AUTO_INIT
+
+		if not self.access:
+			if 'title' in kwargs:
+				self.title, self.year = Utils.title_parser(kwargs['title'])
+				if self.year is None:
+					year_url =  Utils.url_to_name(link)[1]
+					self.year = year_url if year_url.isnumeric() else None
+			else:
+				self.title, self.year = Utils.url_to_name(link)
+
+			for name, value in kwargs.items():
+				if name != 'title':
+					setattr(self, name, value)
+
+
+
+	@property
+	def soup(self):
+		if not hasattr(self, '_soup'):
+			self._soup = Utils.page_downloader(self.link)
+		return self._soup
+
+	@soup.setter
+	def soup(self, value):
+		if value:
+			self._soup = value
