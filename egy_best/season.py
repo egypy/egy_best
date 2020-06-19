c63ce07beb3cceddb85a77171eae2e2b2ed589b6
n0x1s <ilyasselfatimy9@gmail.com>
2020-06-19 14:08:47 +0100

    updated the Actor class to the new style reduce internet consumption

--- egy_best/season.py
+++ egy_best/season.py
@@ -3,20 +3,38 @@ from serie import Serie
 from lib.utils import Utils
 
 class Season(Serie):
-	def __init__(self, link):
-		super().__init__(link)
-		self.episodes = self.get_episodes()
+	def __init__(self, link, **kwargs):
+		super().__init__(link, **kwargs)
 		self.season_number = self.find_order('season')
-		self.serie = Serie((self.get_serie_from_season()))
 
 	def get_episodes(self):
 		""" get all episodes of a Season """
 		container = self.soup.find(class_='movies_small')
-		return [Utils.pickup_class(link['href'])
+		return [Utils.pickup_class(
+					link['href'],
+					title=link.find(class_='title').text,
+					thumbnail_image=link.img['src'],
+				)
 		 	for link in container.find_all('a')]
 
 	def find_order(self, pram):
 		return  re.search(f'{pram}-(.*)/', self.link).group(1)
 
 	def get_serie_from_season(self):
-		return self.soup.find(text=self.serie).find_parent()['href']
+		elem = self.soup.find(class_='nowrap').find_parent().find_all('td')[1]
+		return elem.a['href'], elem.a.text.strip()
+
+	@property
+	def serie(self):
+		if not hasattr(self, '_serie'):
+			data = self.get_serie_from_season()
+			setattr(self, '_serie', Serie(
+					link=data[0],
+					title=data[1]
+					),
+				)
+		return self._serie
+
+	@property
+	def episodes(self):
+		return self.get_episodes()
