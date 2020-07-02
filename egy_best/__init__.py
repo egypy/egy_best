8efde56c15d06b1a4d7d48bc24a0209149e10ead
n0x1s <ilyasselfatimy9@gmail.com>
2020-07-02 01:29:39 +0100

    moved from relative imports to abs imports

---
+++ egy_best/__init__.py
@@ -0,0 +1,6 @@
+from egy_best.actor import Actor
+from egy_best.episode import Episode
+from egy_best.home import Home
+from egy_best.movie import Movie
+from egy_best.season import Season
+from egy_best.serie import Serie
