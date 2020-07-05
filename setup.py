9136cc38e5f1ddc3e3d843e33d4099fd12076fbf
n0x1s <n0x1s0x01@gmail.com>
2020-07-05 22:18:42 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+    name='egybest',
+    packages=['egy_best'],
+    version='0.1',
+    license='MIT',
+    description='An Api for egybest movies website',
+    author='n0x1s',
+    author_email='n0x1s0x01@gmail.com',
+    #url='https://github.com/n0x1s/cached_properties',
+    #download_url='https://github.com/N0x1s/cached_properties/archive/0.7.1.tar.gz',
+    keywords=['movies api', 'actors api', 'scraping movies',
+              'movies db', 'movies'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
