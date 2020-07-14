b08cbf5191adc2d56c16a729584faee3af883cda
n0x1s <ilyasselfatimy9@gmail.com>
2020-07-14 07:54:09 +0100

    added heroku api with FastAPI

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: uvicorn egybest_api.app:app --host=0.0.0.0 --port=${PORT:-5000} 
---
+++ egybest_api.py
@@ -0,0 +1,81 @@
+from fastapi import FastAPI
+from typing import Optional
+from b64 import Tag64
+from base64 import b64encode
+from egy_best import (
+    Site,
+    Actor,
+    Home,
+    Serie,
+    Movie,
+    Season,
+    Episode
+)
+
+app = FastAPI()
+session = Site()
+
+@app.get("/")
+def read_root():
+    return {"error": "NO!"}
+
+@app.get("/search")
+def search(q:str):
+    resutls = session.search(q)
+    content = dict()
+    for i, r in enumerate(resutls):
+        info = r.get_thumbnail_info()
+        # info['thumbnail'] = Tag64(f"https:{info['thumbnail']}", 'img').html_tag
+        info['type'] = r.page_type
+        info['url'] = r.link
+        if r.page_type == 'series':
+            info['seasons'] = f'/seasons?url={r.link}'
+        else:
+            info['downloadinfo'] = f'/downloadinfo?url={r.link}'
+        content.update({i: info})
+    return content
+
+@app.get("/filter/{material}")
+def filter(material, q):
+    # return session.supported
+    if material in session.supported:
+        rs = session.filter(material, q.split('-'))
+        if rs:
+            content = dict()
+            for i, r in enumerate(rs):
+                info = r.get_thumbnail_info()
+                info['type'] = r.page_type
+                info['url'] = r.link
+                if r.page_type == 'series':
+                    info['seasons'] = f'/seasons?url={r.link}'
+                else:
+                    info['downloadinfo'] = f'/downloadinfo?url={r.link}'
+                content.update({i: info})
+            return content
+    return {'error': 'Nothing Has Been Found'}
+    
+
+@app.get("/seasons")
+def seasons(url: str):
+    return {n:
+        {'title':s.title, 'url': s.link, 'episodes': f'/episodes?url={s.link}', 'thumbnail': s.thumbnail} 
+        for n, s in enumerate(Serie(url).seasons)}
+
+@app.get("/episodes")
+def episodes(url: str):
+    return {n: 
+    {
+        'url': s.link,
+        'title':s.title,
+        'thumbnail': s.thumbnail,
+        'downloadinfo': f'/downloadinfo?url={s.link}'
+        } for n, s in enumerate(Season(url).episodes)}
+
+@app.get('/downloadinfo')
+def download(url: str):
+    resutls = {'error': 'Bad Usage'}
+    if 'episode' in url:
+        resutls = Episode(url).get_source_link()
+    elif 'movie' in url:
+        resutls = Movie(url).get_source_link()
+    return resutls
